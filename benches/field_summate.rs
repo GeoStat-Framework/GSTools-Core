6b936cace815997f7e614774a973901d04f4d987
LSchueler <mostem@posteo.de>
2021-09-20 17:31:47 +0200

    Rustfmt

--- benches/field_summate.rs
+++ benches/field_summate.rs
@@ -1,27 +1,35 @@
-use std::path::Path;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-use ndarray::{Array1, Array2, arr2};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use gstools_core::field::summator;
+use ndarray::{arr2, Array1, Array2};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 
-fn read_1d_from_file(file_path: &Path) -> Array1::<f64> {
+fn read_1d_from_file(file_path: &Path) -> Array1<f64> {
     let file = File::open(file_path).expect("File wasn't found");
     let reader = BufReader::new(file);
 
-    reader.lines().map(|line| line.unwrap().parse::<f64>().unwrap()).collect()
+    reader
+        .lines()
+        .map(|line| line.unwrap().parse::<f64>().unwrap())
+        .collect()
 }
 
-fn read_2d_from_file(file_path: &Path) { //-> Array2::<f64> {
+fn read_2d_from_file(file_path: &Path) {
+    //-> Array2::<f64> {
     let file = File::open(file_path).expect("File wasn't found");
     let reader = BufReader::new(file);
 
     let mut data = Vec::new();
 
-    let r: Vec<Vec<f64>> = reader.lines()
-        .map(|line| line.unwrap().split(char::is_whitespace)
-            .map(|number| number.parse::<f64>().unwrap())
-            .collect())
+    let r: Vec<Vec<f64>> = reader
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .split(char::is_whitespace)
+                .map(|number| number.parse::<f64>().unwrap())
+                .collect()
+        })
         .collect();
     let r = arr2(&r);
 
@@ -39,7 +47,6 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     let z_1 = read_1d_from_file(&path.join("field_z_1.txt"));
     let z_2 = read_1d_from_file(&path.join("field_z_2.txt"));
-
 
     //c.bench_function("1st test", |b| b.iter(|| black_box(20)));
 }
