8d6bcd04143dcd7f0c21b1a05bb671ab68112b6f
LSchueler <mostem@posteo.de>
2021-09-22 17:29:50 +0200

    Add benchmarks for field summators

--- benches/field_summate.rs
+++ benches/field_summate.rs
@@ -1,6 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use gstools_core::field::summator;
-use ndarray::{arr2, Array1, Array2};
+use criterion::{criterion_group, criterion_main, Criterion};
+use gstools_core::field::{summator, summator_incompr};
+use ndarray::{stack, Array1, Array2, Axis};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
@@ -15,40 +15,48 @@ fn read_1d_from_file(file_path: &Path) -> Array1<f64> {
         .collect()
 }
 
-fn read_2d_from_file(file_path: &Path) {
-    //-> Array2::<f64> {
+fn read_2d_from_file(file_path: &Path) -> Array2<f64> {
     let file = File::open(file_path).expect("File wasn't found");
     let reader = BufReader::new(file);
 
-    let mut data = Vec::new();
-
-    let r: Vec<Vec<f64>> = reader
+    let vec: Vec<Array1<f64>> = reader
         .lines()
-        .map(|line| {
-            line.unwrap()
+        .map(|l| {
+            l.unwrap()
                 .split(char::is_whitespace)
                 .map(|number| number.parse::<f64>().unwrap())
                 .collect()
         })
         .collect();
-    let r = arr2(&r);
 
-    println!("r = {:?}", r);
+    let shape = (vec.len(), vec[0].dim());
+
+    let flat_vec: Vec<f64> = vec.iter().flatten().cloned().collect();
+
+    Array2::from_shape_vec(shape, flat_vec).unwrap()
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let path = Path::new("benches/input");
 
-    let x = read_1d_from_file(&path.join("field_x.txt"));
-    let y = read_1d_from_file(&path.join("field_y.txt"));
-    let z = read_1d_from_file(&path.join("field_z.txt"));
+    let x = read_1d_from_file(&path.join("field_bench_x.txt"));
+    let y = read_1d_from_file(&path.join("field_bench_y.txt"));
+    let z = read_1d_from_file(&path.join("field_bench_z.txt"));
 
-    let cov_samples = read_2d_from_file(&path.join("field_cov_samples.txt"));
+    let pos = stack![Axis(0), x, y, z];
 
-    let z_1 = read_1d_from_file(&path.join("field_z_1.txt"));
-    let z_2 = read_1d_from_file(&path.join("field_z_2.txt"));
+    let cov_samples = read_2d_from_file(&path.join("field_bench_cov_samples.txt"));
 
-    //c.bench_function("1st test", |b| b.iter(|| black_box(20)));
+    let z_1 = read_1d_from_file(&path.join("field_bench_z_1.txt"));
+    let z_2 = read_1d_from_file(&path.join("field_bench_z_2.txt"));
+
+    c.bench_function("field summate", |b| {
+        b.iter(|| summator(cov_samples.view(), z_1.view(), z_2.view(), pos.view()))
+    });
+
+    c.bench_function("field summate incompr", |b| {
+        b.iter(|| summator_incompr(cov_samples.view(), z_1.view(), z_2.view(), pos.view()))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/gen_benchmark_inputs.py
+++ benches/gen_benchmark_inputs.py
@@ -4,13 +4,11 @@
 from pathlib import Path
 import numpy as np
 from gstools import Gaussian
-from gstools.field.generator import RandMeth
+from gstools.field.generator import RandMeth, IncomprRandMeth
 
 
-if __name__ == '__main__':
-    path = Path('input')
-    seed = 19031977
-    pos_no = 10
+def gen_field_summate(path, seed):
+    pos_no = 1000
     mode_no = 100
     x = np.linspace(0.0, 10.0, pos_no)
     y = np.linspace(-5.0, 5.0, pos_no)
@@ -19,9 +17,32 @@ if __name__ == '__main__':
     model = Gaussian(dim=3, var=1.0, len_scale=1.0)
 
     rm = RandMeth(model, mode_no, seed)
-    np.savetxt(path / 'field_x.txt', x)
-    np.savetxt(path / 'field_y.txt', y)
-    np.savetxt(path / 'field_z.txt', z)
-    np.savetxt(path / 'field_cov_samples.txt', rm._cov_sample)
-    np.savetxt(path / 'field_z_1.txt', rm._z_1)
-    np.savetxt(path / 'field_z_2.txt', rm._z_2)
+    np.savetxt(path / 'field_bench_x.txt', x)
+    np.savetxt(path / 'field_bench_y.txt', y)
+    np.savetxt(path / 'field_bench_z.txt', z)
+    np.savetxt(path / 'field_bench_cov_samples.txt', rm._cov_sample)
+    np.savetxt(path / 'field_bench_z_1.txt', rm._z_1)
+    np.savetxt(path / 'field_bench_z_2.txt', rm._z_2)
+
+    pos_no = 100000
+    mode_no = 1000
+    x = np.linspace(0.0, 10.0, pos_no)
+    y = np.linspace(-5.0, 5.0, pos_no)
+    z = np.linspace(-6.0, 8.0, pos_no)
+
+    model = Gaussian(dim=3, var=1.0, len_scale=1.0)
+
+    rm = RandMeth(model, mode_no, seed)
+    np.savetxt(path / 'field_vs_x.txt', x)
+    np.savetxt(path / 'field_vs_y.txt', y)
+    np.savetxt(path / 'field_vs_z.txt', z)
+    np.savetxt(path / 'field_vs_cov_samples.txt', rm._cov_sample)
+    np.savetxt(path / 'field_vs_z_1.txt', rm._z_1)
+    np.savetxt(path / 'field_vs_z_2.txt', rm._z_2)
+
+
+if __name__ == '__main__':
+    path = Path('input')
+    seed = 19031977
+
+    gen_field_summate(path, seed)
