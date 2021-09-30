ee26e77e45b7963f7958dd974e95c672affbc265
LSchueler <lennart.schueler@ufz.de>
2021-09-30 19:08:44 +0200

    Add Python script to compare Cython, Rust performance

---
+++ benches/compare_cython_rust.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import cProfile
+from pathlib import Path
+import numpy as np
+import gstools as gs
+from gstools.field.generator import RandMeth
+from gstools.field.summator import summate, summate_incompr
+from gstools.krige.krigesum import calc_field_krige_and_variance, calc_field_krige
+from gstools.variogram.estimator import (
+    structured,
+    ma_structured,
+    unstructured,
+    directional,
+)
+import gstools_core as gsc
+
+
+if __name__ == '__main__':
+    path = Path('input')
+
+    ##########################################################################
+
+    x = np.loadtxt(path / 'field_vs_x.txt')
+    y = np.loadtxt(path / 'field_vs_y.txt')
+    z = np.loadtxt(path / 'field_vs_z.txt')
+    cov_samples = np.loadtxt(path / 'field_vs_cov_samples.txt')
+    z_1 = np.loadtxt(path / 'field_vs_z_1.txt')
+    z_2 = np.loadtxt(path / 'field_vs_z_2.txt')
+    pos = np.array((x, y, z))
+
+    print(f'SUMMATE')
+    print(f'\tCYTHON IMPLEMENTATION')
+    cProfile.run(
+        'summate(cov_samples, z_1, z_2, pos)'
+    )
+
+    print(f'\tRUST IMPLEMENTATION')
+    cProfile.run(
+        'gsc.summate(cov_samples, z_1, z_2, pos)'
+    )
+
+    print(f'SUMMATE INCOMPR')
+    print(f'\tCYTHON IMPLEMENTATION')
+    cProfile.run(
+        'summate_incompr(cov_samples, z_1, z_2, pos)'
+    )
+
+    print(f'\tRUST IMPLEMENTATION')
+    cProfile.run(
+        'gsc.summate_incompr(cov_samples, z_1, z_2, pos)'
+    )
+
+    ##########################################################################
+
+    krige_mat = np.loadtxt(path / 'krige_vs_krige_mat.txt')
+    k_vec = np.loadtxt(path / 'krige_vs_k_vec.txt')
+    krige_cond = np.loadtxt(path / 'krige_vs_krige_cond.txt')
+
+    print(f'KRIGE_AND_VAR')
+    print(f'\tCYTHON IMPLEMENTATION')
+    cProfile.run(
+        'calc_field_krige_and_variance(krige_mat, k_vec, krige_cond)'
+    )
+
+    print(f'\tRUST IMPLEMENTATION')
+    cProfile.run(
+        'gsc.calc_field_krige_and_variance(krige_mat, k_vec, krige_cond)'
+    )
+    
+    print(f'KRIGE')
+    print(f'\tCYTHON IMPLEMENTATION')
+    cProfile.run(
+        'calc_field_krige(krige_mat, k_vec, krige_cond)'
+    )
+
+    print(f'\tRUST IMPLEMENTATION')
+    cProfile.run(
+        'gsc.calc_field_krige(krige_mat, k_vec, krige_cond)'
+    )
