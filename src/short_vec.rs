2df2cb87dd00464fed8caa56612a3596a9fb5239
Adam Reichold <adam.reichold@t-online.de>
2021-10-26 09:01:59 +0200

    Add a minimal amount of documentation for the newly introduced ShortVec type.

--- src/short_vec.rs
+++ src/short_vec.rs
@@ -3,15 +3,20 @@ use std::ops::{Index, IndexMut};
 
 use ndarray::ArrayView1;
 
+/// Represents an element of the vector space R^N with N fixed at compile time.
+///
+/// The increased alignment to 16 bytes makes these amenable for SIMD processing through auto-vectorization.
 #[derive(Clone, Copy)]
 #[repr(align(16))]
 pub struct ShortVec<const N: usize>([f64; N]);
 
 impl<const N: usize> ShortVec<N> {
+    /// Create an instance of the origin, i.e. a vector with all components equal to zero.
     pub fn zero() -> Self {
         Self([0.0; N])
     }
 
+    /// Create an instance whose components are the first N values of the given one-dimensional array view.
     pub fn from_array(array: ArrayView1<'_, f64>) -> Self {
         let mut this = Self::zero();
 
@@ -22,10 +27,12 @@ impl<const N: usize> ShortVec<N> {
         this
     }
 
+    /// Compute the inner product of `self` with `other`.
     pub fn dot(&self, other: &Self) -> f64 {
         (0..N).map(|idx| self.0[idx] * other.0[idx]).sum()
     }
 
+    /// Add `other` to `self` which is updated in-place.
     pub fn add(&mut self, other: &Self) {
         (0..N).for_each(|idx| {
             self.0[idx] += other.0[idx];
